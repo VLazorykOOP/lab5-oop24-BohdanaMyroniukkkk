ca9e803f5db2bb82609aa11bda1a99bd110bb3d7
myroniukkk <myroniukkk@DESKTOP-33BST9I>
2024-05-10 11:04:17 +0300

    перше завдання

--- OOPLab5T/OOPLab5T.cpp
+++ OOPLab5T/OOPLab5T.cpp
@@ -1,24 +1,95 @@
-﻿// OOPLab5T.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <string>
 
+using namespace std;
 
-#include <iostream>
+// Клас "Точка"
+class Point {
+protected:
+    int x, y;
 
-// Ваші файли загловки 
-//
-#include "Lab5Exmaple.h"
-int main()
-{
-    std::cout << " Lab #5  !\n";
-    //  Код виконання завдань
-    //  Головне меню завдань
-    //  Функції та класи можуть знаходитись в інших файлах проекту
+public:
+    // Конструктор за замовчуванням
+    Point() : x(0), y(0) {}
 
-    int chain = 1;
-    if (chain == 1) chain = mainExample1();
-    if (chain == 2) chain = mainExample2();
-    if (chain == 3) chain = mainExample3();
-    if (chain == 4) chain = mainExample4();
-    if (chain == 5) chain = mainExample5();
+    // Конструктор з параметрами
+    Point(int xCoord, int yCoord) : x(xCoord), y(yCoord) {}
 
+    // Деструктор
+    ~Point() {}
+
+    // Функція друку
+    void print() {
+        cout << "Point: (" << x << ", " << y << ")" << endl;
+    }
+
+    // Функція перепризначення координат точки
+    void setCoordinates(int newX, int newY) {
+        x = newX;
+        y = newY;
+    }
+};
+
+// Клас "Кольорова точка"
+class ColoredPoint : public Point {
+private:
+    string color;
+
+public:
+    // Конструктор за замовчуванням
+    ColoredPoint() : Point(), color("black") {}
+
+    // Конструктор з параметрами
+    ColoredPoint(int xCoord, int yCoord, string clr) : Point(xCoord, yCoord), color(clr) {}
+
+    // Деструктор
+    ~ColoredPoint() {}
+
+    // Функція друку
+    void print() {
+        cout << "Colored Point: (" << x << ", " << y << "), Color: " << color << endl;
+    }
+
+    // Функція перепризначення кольору
+    void setColor(string newColor) {
+        color = newColor;
+    }
+
+    // Функція виведення координат точки
+    void printCoordinates() {
+        cout << "Coordinates: (" << x << ", " << y << ")" << endl;
+    }
+};
+
+int main() {
+    // Тестування класу "Точка"
+    Point p1;
+    cout << "Point 1:" << endl;
+    p1.print();
+
+    Point p2(3, 4);
+    cout << "Point 2:" << endl;
+    p2.print();
+
+    p2.setCoordinates(5, 6);
+    cout << "Point 2 after setting new coordinates:" << endl;
+    p2.print();
+
+    // Тестування класу "Кольорова точка"
+    ColoredPoint cp1;
+    cout << "Colored Point 1:" << endl;
+    cp1.print();
+
+    ColoredPoint cp2(1, 2, "red");
+    cout << "Colored Point 2:" << endl;
+    cp2.print();
+
+    cp2.setColor("blue");
+    cout << "Colored Point 2 after changing color:" << endl;
+    cp2.print();
+
+    cout << "Coordinates of Colored Point 2:" << endl;
+    cp2.printCoordinates();
+
+    return 0;
 }
